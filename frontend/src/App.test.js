f6447dd7ce52dd1c4796da33238515708a482c08
Abdelmoula AMAHANE <AMAHANE@sypex.local>
2026-07-11 20:28:26 +0000

    Configure frontend tests and SonarQube

--- frontend/src/App.test.js
+++ frontend/src/App.test.js
@@ -1,8 +1,17 @@
-import { render, screen } from '@testing-library/react';
-import App from './App';
+import { render, screen } from "@testing-library/react";
+import App from "./App";
 
-test('renders learn react link', () => {
+test("affiche correctement la page de connexion", () => {
   render(<App />);
-  const linkElement = screen.getByText(/learn react/i);
-  expect(linkElement).toBeInTheDocument();
+
+  expect(
+    screen.getByRole("heading", { name: /HR MANAGEMENT/i })
+  ).toBeInTheDocument();
+
+  expect(screen.getByLabelText(/Email/i)).toBeInTheDocument();
+  expect(screen.getByLabelText(/Mot de passe/i)).toBeInTheDocument();
+
+  expect(
+    screen.getByRole("button", { name: /Se connecter/i })
+  ).toBeInTheDocument();
 });
